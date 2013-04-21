b4efe139f6a8cb19b50617a0230b03b92efc1fa8
billhsu <billhsu.x@gmail.com>
2013-04-21 23:20:32 +0800

    vector->map. Need to be done

--- RayTracer/src/Audio/hrtf.h
+++ RayTracer/src/Audio/hrtf.h
@@ -6,12 +6,19 @@
 #ifndef HRTF_H
 #define HRTF_H
 #include <string>
+#include <vector>
 class hrtf
 {
 public:
     hrtf(void);
     ~hrtf(void);
     void load(std::string dir);
+
+private:
+    struct hrtf_data{
+        float ir[128];
+    };
+    std::vector<hrtf_data> a;
 };
 
 #endif
